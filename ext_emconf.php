ac0eef2a0b8c7e5ee40573f60b7163d1e2e7fea4
Stefanos Karasavvidis <sk@karasavvidis.gr>
2023-02-12 15:25:19 +0200

    support TYPO3 11. Upgrade glightbox to 3.2.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,11 +8,11 @@ $EM_CONF[$_EXTKEY] = array(
   'state' => 'beta',
   'uploadfolder' => false,
   'createDirs' => '',
-  'version' => '0.1.0',
+  'version' => '0.2.0',
   'constraints' => [
     'depends' => [
-        'typo3' => '9.5.0-10.9.99',
-        'fluid_styled_content' => '9.5.0-10.9.99'
+        'typo3' => '11.5.0-11.99.99',
+        'fluid_styled_content' => '11.5.0-11.99.99'
     ],
     'conflicts' => [],
     'suggests' => []
